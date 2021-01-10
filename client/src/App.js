5bc877117a2670d6402b59ced84ac9055ef34151
Tiberiu Andrei <tiberiu.andrei858@gmail.com>
2021-01-10 11:48:13 +0200

    user tanble

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import AnnouncementPage from './components/announcement/AnnouncementPage'
+import UserTable from './components/user/UserTable'
+import CreateUserForm from './components/user/CreateUserForm'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useEffect } from 'react'
 
 function App() {
   return (
-    <AnnouncementPage />
+    //<AnnouncementPage />
+    <BrowserRouter>
+      <Switch>
+        <Route path = "/createUser" component = {CreateUserForm}/>
+        <Route path = "/users/:firstName/:lastName/:email/:phoneNumber/:address" component = {UserTable}/>
+        <Route path = "/users" component = {UserTable}/>
+        <Route exact strict path = "/" component = {AnnouncementPage}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
