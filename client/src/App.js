d6a612e767d3d48641b88016b0f6e746e0e7d438
Tiberiu Andrei <tiberiu.andrei858@gmail.com>
2021-01-12 17:25:31 +0200

    Test

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,27 @@ import './App.css';
 import AnnouncementPage from './components/announcement/AnnouncementPage'
 import UserTable from './components/user/UserTable'
 import CreateUserForm from './components/user/CreateUserForm'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { useEffect } from 'react'
+import WelcomePage from './components/WelcomePage'
+import Login from './components/Login'
 
 function App() {
   return (
     //<AnnouncementPage />
     <BrowserRouter>
       <Switch>
-        <Route path = "/createUser" component = {CreateUserForm}/>
-        <Route path = "/users/:firstName/:lastName/:email/:phoneNumber/:address" component = {UserTable}/>
-        <Route path = "/users" component = {UserTable}/>
-        <Route exact strict path = "/" component = {AnnouncementPage}/>
+      <Route path = "/welcomepage/:email" component={WelcomePage}/>
+      <Route path = "/" component = {Login}/>
       </Switch>
     </BrowserRouter>
+
+
+
+/*<Route path = "/createUser" component = {CreateUserForm}/>
+        <Route path = "/users/:firstName/:lastName/:email/:phoneNumber/:address" component = {UserTable}/>
+        <Route path = "/users" component = {UserTable}/>
+        <Route exact strict path = "/" component = {AnnouncementPage}/>*/
   );
 }
 
